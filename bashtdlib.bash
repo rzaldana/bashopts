860aee5a4ee44c91d6b01a009d736bab297f586f
raul.zaldana <raul.zaldana@irdeto.com>
2023-01-12 11:19:05 -0600

    add sourced function

--- bashtdlib.bash
+++ bashtdlib.bash
@@ -2,3 +2,21 @@ function hello_world {
   echo "hello, world!"
 }
 
+function sourced {
+  # Returns:
+  #   0 if the function is called in a file that's being sourced
+  #   1 if the function is called in a file that's being executed
+  [[ ${FUNCNAME[1]} == source ]]
+}
+
+main () {
+  hello_world
+}
+
+
+# Exit and don't run main if script is being sourced
+sourced && return
+
+main "$@"
+
+
--- tests/bashtdlib_shpec.bash
+++ tests/bashtdlib_shpec.bash
@@ -5,11 +5,46 @@ if [[ "$OS_NAME" == "Darwin" ]]; then
   alias readlink=greadlink
 fi
 
-source "$(dirname "$(readlink -f "$BASH_SOURCE")")"/../bashtdlib.bash
+bashtdlib="$(dirname "$(readlink -f "$BASH_SOURCE")")"/../bashtdlib.bash
 
-describe bashtdlib
-  it "says 'hello world!'"
-    result=$(hello_world)
-    assert equal "hello, world!" "$result"
+describe sourced
+  it "returns true when in a file that's being sourced"
+    # Create temporary file
+    file=$(mktemp) || return
+
+    # Set contents of file so that it sources bashtdlib
+    # and then calls the function called 'sourced'
+    printf 'source "%s"\nsourced' "$bashtdlib" > "$file"
+
+    # Source the file in a subshell
+    (source $file)
+
+    # Check that the 'sourced' function returns 0
+    assert equal 0 $?
+
+    # Remove temporary file
+    rm $file
   end
+
+  it "returns false when in a file that's being executed"
+    # Create temporary file
+    file=$(mktemp) || return
+
+    # Set contents of file so that it sources bashtdlib
+    # and then calls the function called 'sourced'
+    printf 'source "%s"\nsourced' "$bashtdlib" > "$file"
+
+    # Make the tmp file executable
+    chmod 755 "$file"
+
+    # Run the file directly
+    "$file"
+
+    # Check that the 'sourced' function does NOT return 0
+    assert unequal 0 $?
+
+    # Remove temporary file
+    rm "$file"
+  end
+
 end
